dd6131f61d37f446682e8f3583656ea8461fe329
fistonsano04 <sanofiston04@gmail.com>
2025-05-10 14:33:27 +0200

    adding register page

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Authantication Page</title>
+    <title>Authentication Page</title>
 <link rel="stylesheet" href="{{ asset('assets/css/auth.css') }}">
 </head>
 
